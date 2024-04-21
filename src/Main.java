7551b114bc3eda93ba6d09c98100a3a21fba5f62
AnCapone <okai.kot44@gmail.com>
2024-04-21 19:26:16 +0300

    Implement task 3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        findUnique(toList(new Integer[]{42, 17, 28, 17, 64, 9, 73, 51, 36, 73, 64, 9}));
 
     }
 
@@ -18,9 +16,28 @@ public class Main {
         return count;
     }
 
-    public static <T> List<T> toList(T[] arr) {
-        List<T> list = new ArrayList<>();
+    public static <T> ArrayList<T> toList(T[] arr) {
+        ArrayList<T> list = new ArrayList<>();
         Collections.addAll(list, arr);
         return list;
+    }
+
+    public static void findUnique(ArrayList<Integer> inputList) {
+        Set<Integer> inputSet = new HashSet<>(inputList);
+        List<Integer> uniqueList = new ArrayList<>();
+
+        for (Integer numberInSet : inputSet) {
+            int count = 0;
+            for (Integer num : inputList) {
+                if (numberInSet.equals(num)) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                uniqueList.add(numberInSet);
+            }
+        }
+
+        System.out.println(uniqueList);
     }
 }
