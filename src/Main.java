a44fa152e987f0d0a98c6bc847323077450317b9
AnCapone <okai.kot44@gmail.com>
2024-04-21 17:49:23 +0300

    Implement task 1, 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
     }
 
+    public static int countOccurance(ArrayList<String> list, String word) {
+        int count = 0;
+        for (String s : list) {
+            if (s.equals(word)) {
+                count++;
+            }
+        }
+        return count;
+    }
 
+    public static <T> List<T> toList(T[] arr) {
+        List<T> list = new ArrayList<>();
+        Collections.addAll(list, arr);
+        return list;
+    }
 }
